8fe0aef167048ed3a88d169cafc55901c187e588
Gopikrishnakonakanchi <konakanchigopikrishna96@gmail.com>
2025-02-03 18:26:23 -0600

    This is Addition Project

---
+++ Addition.java
@@ -0,0 +1,12 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 40;
+        int b = 5;
+        int c = a+b;
+        int d = a-b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
